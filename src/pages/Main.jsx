57ed626211cee9f6cbe1be7f52e093f37b3bff0c
jeehwan <tjtojan@naver.com>
2024-04-15 23:48:34 +0900

    FEAT: Main 페이지에서 Detail 페이지로 라우팅 구현

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 function Main() {
-  return <div>Main</div>;
+  const [postList, setPostList] = useState([]);
+
+  const fetchPostList = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8080/post/list`);
+
+      setPostList(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPostList();
+  }, []);
+
+  return (
+    <ul>
+      {postList.map((post) => (
+        <ol>
+          <Link to={`/post/detail/${post.id}`}>
+            {post.id}. {post.title}
+          </Link>
+        </ol>
+      ))}
+    </ul>
+  );
 }
 
 export default Main;
